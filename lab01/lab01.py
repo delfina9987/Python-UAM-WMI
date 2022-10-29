a6f4320ee8544ffb13ddab0149a912c4f7eff463
Delfina <68329872+delfina9987@users.noreply.github.com>
2022-10-29 19:00:48 +0200

    Add files via upload

---
+++ lab01/lab01.py
@@ -0,0 +1,152 @@
+# Zadanie 1
+'''print("Hello World!")'''
+
+# Zadanie 2
+'''print("Hello World!\nDelfina")'''
+
+# Zadanie 3
+'''
+sep = '---'
+end = 'KONIEC'
+print('a' + sep + 'b' + sep + 'c' + sep + end)
+'''
+
+# Zadanie 4
+'''
+sep = '-***-'
+end = '-****'
+print('1' + sep + '2' + sep + '3' + sep + '4' + end)
+'''
+
+# Zadanie 5
+'''
+sep = ' & '
+end = ' &&&'
+print('Ala' + sep + 'ma' + sep + 'kota' + end)
+'''
+
+# Zadanie 6
+'''
+name = input()
+print('Cześć '+ name + '!')
+'''
+
+# Zadanie 7
+'''
+n = int(input())
+m = int(input())
+sum = n+m
+print(sum)
+'''
+
+# Zadanie 8
+'''
+A = int(input())
+B = int(input())
+print('roznica = ', A-B)
+'''
+
+# Zadanie 9
+'''
+A = int(input())
+B = int(input())
+pow = A**2 + B**2
+print(pow)
+'''
+
+# Zadanie 10
+'''
+A = int(input())
+B = int(input())
+C = int(input())
+pole = 2*A*B + 2*A*C + 2*B*C
+print('P = ', pole)
+'''
+
+# Zadanie 11
+'''
+A = int(input())
+B = int(input())
+C = int(input())
+delta = B**2 - 4*A*C
+print(delta)
+'''
+
+# Zadanie 12
+'''
+a = int(input())
+b = int(input())
+print('a =',a)
+print('b =',b)
+print('a + b =',a+b)
+print('a - b =',a-b)
+print('a * b =',a*b)
+print('a / b =',a/b)
+print('a // b =',a//b)
+'''
+
+# Zadanie 13
+'''
+a = int(input())
+b = int(input())
+print(a//b)
+'''
+
+# Zadanie 14
+'''
+a = int(input())
+b = int(input())
+print(a**b)
+'''
+
+# Zadanie 15
+'''
+A = int(input())
+B = int(input())
+C = int(input())
+sum = A+B+C
+sr = sum/3
+print('suma =', sum)
+print('srednia =', sr)
+'''
+
+# Zadanie 16
+'''
+a = int(input())
+b = int(input())
+h = int(input())
+pole = (a+b)*h/2
+print('Pole = ', pole)
+'''
+
+# Zadanie 17
+'''
+t = int(input())
+s = 1320
+v = s/t
+print('v = ', v, 'km/h')
+'''
+
+# Zadanie 18
+'''
+mila = 1609
+km = int(input())
+print(km*mila, 'km')
+'''
+
+# Zadanie 19
+#t = int(input()) #czas przejazdu pociągu  godzinach
+
+
+# Zadanie 20
+'''
+warsaw_time = int(input())
+time_difference = 6
+print(warsaw_time-time_difference)
+'''
+
+# Zadanie 21
+'''
+n = int(input())
+print(n**2)
+'''
